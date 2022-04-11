25c2a817140288f5b6038a97cb965019318a2854
szelos <szelos@intellisoft.gr>
2022-04-11 13:31:37 +0300

    jQuery added and used to produce body element

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 var bookListingTemplate = require("./book-listing.handlebars");
 import data from './data/data.json'
 
-document.addEventListener("DOMContentLoaded", function() {
-	var div = document.createElement('div');
-	div.innerHTML = bookListingTemplate(data);
-	document.body.appendChild(div);
-});
+// document.addEventListener("DOMContentLoaded", function() {
+// 	var div = document.createElement('div');
+// 	div.innerHTML = bookListingTemplate(data);
+// 	document.body.appendChild(div);
+// });
+
+$(() => {
+	var div = $(`<div id="mitsos"></div>`)
+	.html(bookListingTemplate(data))
+	.appendTo("body");
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path"),
   webpack = require("webpack"),
   HTMLWebpackPlugin = require('html-webpack-plugin'),
-  { CleanWebpackPlugin } = require('clean-webpack-plugin');
+  { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+  TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: "./src/app.js",
@@ -12,26 +13,13 @@ module.exports = {
 
   //see: https://webpack.js.org/configuration/dev-server/
   //see: https://github.com/webpack/webpack-dev-server
-  devServer: { 
+  devServer: {
     compress: true,
     port: 9000,
-    hot:true,
-    open:true,
-    liveReload:true
+    hot: true,
+    open: true,
+    liveReload: true
   },
-
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: "Test App", 
-      template: "./src/index.html", //handlebar file 
-      filename: 'index.html'
-    }),
-    new CleanWebpackPlugin(
-      {
-        cleanOnceBeforeBuildPatterns: ['**/*', path.join(process.cwd(), 'extra/**/*')]
-      }
-    )
-  ],
 
   module: {
     rules: [
@@ -48,5 +36,38 @@ module.exports = {
         }
       }
     ]
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Test App",
+      template: "./src/index.html", //handlebar file 
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*', path.join(process.cwd(), 'extra/**/*')]
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    })
+  ],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false, // prevents TerserPlugin from extracting a [chunkName].js.LICENSE.txt file
+        terserOptions: {
+          format: {
+            // Tell terser to remove all comments except for the banner added via LicenseWebpackPlugin.
+            // This can be customized further to allow other types of comments to show up in the final js file as well.
+            // See the terser documentation for format.comments options for more details.
+            comments: (astNode, comment) => (comment.value.startsWith('! licenses are at '))
+          }
+        }
+      })
+    ]
   }
+
 };
